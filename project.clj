3ece667f49718d50d0eddae2725f9e2d682baf5d
Giacomo Cosenza <giacomo.cosenza@sinapsi.com>
2012-11-15 18:03:03 +0100

    added tutorial 03

--- project.clj
+++ project.clj
@@ -3,19 +3,25 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  ; CLJ source code path
+  ; clojure source code pathname
   :source-paths ["src/clj"]
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  ; lein-cljsbuild plugin to build a CLJS project
-  :plugins [[lein-cljsbuild "0.2.9"]]
-  ; cljsbuild options configuration
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 ; compojure dependency
+                 [compojure "1.1.3"]]
+  :plugins [; cljsbuild plugin
+            [lein-cljsbuild "0.2.9"]
+            ; ring plugin
+            [lein-ring "0.7.5"]]
+  ; ring tasks configuration
+  :ring {:handler modern-cljs.core/handler}
+  ; cljsbuild tasks configuration
   :cljsbuild {:builds
-              [{; CLJS source code path
+              [{; clojurescript source code path
                 :source-path "src/cljs"
-                ; Google Closure (CLS) options configuration
-                :compiler {; CLS generated JS script filename
+                ; Google Closure Compiler options
+                :compiler {; the name of the emitted JS file
                            :output-to "resources/public/js/modern.js"
-                           ; minimal JS optimization directive
+                           ; minimum optimization
                            :optimizations :whitespace
-                           ; generated JS code prettyfication
+                           ; prettyfying emitted JS
                            :pretty-print true}}]})
